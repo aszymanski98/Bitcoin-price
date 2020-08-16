7de13e44e79ee7405b1d03d52f205f3cb999a43a
Adam Szymański <adam.szymanski@protonmail.com>
2020-08-16 22:44:37 +0200

    added filter option

---
+++ src/CryptoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './CryptoList.css';
+
+const CryptoList = (props) => {
+
+    let cryptoList = props.cryptoList;
+
+    let liElements = cryptoList.map((cryptoObj) => {
+        return (
+            <li key={cryptoObj.currency}>
+            <span className="label">Last rate: </span>
+            <span className={`rate ${cryptoObj.cssClass}`}>{cryptoObj.lastRate} {cryptoObj.arrow}</span>
+            <span className="currencyName">{cryptoObj.currency}</span>
+            <span className="currencySymbol">[{cryptoObj.symbol}]</span>
+            </li>
+        );
+    })
+
+return (
+    <div className='crypto-list'>
+        <ul className='list'>
+            {liElements}
+        </ul>
+    </div>
+);
+}
+export default CryptoList;
