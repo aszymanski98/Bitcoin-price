18a7369a68f6cacee56e9b207d29f996fc39922e
Adam Szymański <adam.szymanski@protonmail.com>
2020-08-16 23:50:26 +0200

    Updates

---
+++ precache-manifest.ad793a04ed6a8b363d9d6059fee898cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595c1e22e52ed7cc3aa3b9b4986531bc",
+    "url": "/Crypto-rate-ReactJS/index.html"
+  },
+  {
+    "revision": "99091b4d19f07fdc50c5",
+    "url": "/Crypto-rate-ReactJS/static/css/main.0568c1ff.chunk.css"
+  },
+  {
+    "revision": "e300dddb360287522cc1",
+    "url": "/Crypto-rate-ReactJS/static/js/2.ef495069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Crypto-rate-ReactJS/static/js/2.ef495069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99091b4d19f07fdc50c5",
+    "url": "/Crypto-rate-ReactJS/static/js/main.02a96438.chunk.js"
+  },
+  {
+    "revision": "45ee39e1422bcc376b48",
+    "url": "/Crypto-rate-ReactJS/static/js/runtime-main.9e12ab41.js"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/Crypto-rate-ReactJS/static/media/logo.d7876d48.svg"
+  }
+]);
