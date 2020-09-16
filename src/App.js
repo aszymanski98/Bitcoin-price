ded872b52821e70532edaf8f858bd32d33211d2b
aszymanski98 <adam.szymanski@protonmail.com>
2020-09-16 20:29:19 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Crypto from './Components/Crypto';
+import Crypto from "./Components/Crypto";
 
 function App() {
   return (
--- src/Components/CryptoList.js
+++ src/Components/CryptoList.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import '../Styles/CryptoList.css';
+import "../Styles/CryptoList.css";
 
 const CryptoList = (props) => {
+  let cryptoList = props.cryptoList;
 
-    let cryptoList = props.cryptoList;
+  let liElements = cryptoList.map((cryptoObj) => {
+    return (
+      <li key={cryptoObj.currency}>
+        <span className="label">Last rate: </span>
+        <span className={`rate ${cryptoObj.cssClass}`}>
+          {cryptoObj.lastRate} {cryptoObj.arrow}
+        </span>
+        <span className="currencyName">{cryptoObj.currency}</span>
+        <span className="currencySymbol">[{cryptoObj.symbol}]</span>
+      </li>
+    );
+  });
 
-    let liElements = cryptoList.map((cryptoObj) => {
-        return (
-            <li key={cryptoObj.currency}>
-            <span className="label">Last rate: </span>
-            <span className={`rate ${cryptoObj.cssClass}`}>{cryptoObj.lastRate} {cryptoObj.arrow}</span>
-            <span className="currencyName">{cryptoObj.currency}</span>
-            <span className="currencySymbol">[{cryptoObj.symbol}]</span>
-            </li>
-        );
-    })
-
-return (
-    <div className='crypto-list'>
-        <ul className='list'>
-            {liElements}
-        </ul>
+  return (
+    <div className="crypto-list">
+      <ul className="list">{liElements}</ul>
     </div>
-);
-}
+  );
+};
 export default CryptoList;
