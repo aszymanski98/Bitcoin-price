1a1dd4cb4b488c1e4b6799ea81e459764b591f92
aszymanski98 <adam.szymanski@protonmail.com>
2020-09-17 22:49:11 +0200

    Updates

---
+++ precache-manifest.5ee30d82376af5940e04a42ca7843e6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75f30f7aa01dfc02aa7fd7d71ac3a63",
+    "url": "/Bitcoin-price-ReactJS/index.html"
+  },
+  {
+    "revision": "2b29523c7488f512c515",
+    "url": "/Bitcoin-price-ReactJS/static/css/main.34af5dae.chunk.css"
+  },
+  {
+    "revision": "e300dddb360287522cc1",
+    "url": "/Bitcoin-price-ReactJS/static/js/2.ef495069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bitcoin-price-ReactJS/static/js/2.ef495069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b29523c7488f512c515",
+    "url": "/Bitcoin-price-ReactJS/static/js/main.be473a6c.chunk.js"
+  },
+  {
+    "revision": "6e6ab78a05a59236dce5",
+    "url": "/Bitcoin-price-ReactJS/static/js/runtime-main.c722ad1d.js"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/Bitcoin-price-ReactJS/static/media/logo.d7876d48.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crypto-rate-ReactJS/precache-manifest.b9aac24cc532ca08225bc74e13c48c13.js"
+  "/Bitcoin-price-ReactJS/precache-manifest.5ee30d82376af5940e04a42ca7843e6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crypto-rate-ReactJS/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Bitcoin-price-ReactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
